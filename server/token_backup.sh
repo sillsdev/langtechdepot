f3f54abc54ea0590e7d4d67c0bcc42bb63b55698
JimH44 <jim.henderson44@gmail.com>
2026-09-07 22:17:36 -0700

    s/token_backup.sh for backing up the tokens every day

---
+++ server/token_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# set -x
+# Get the current date for the filename
+DATE=$(date +%Y-%m-%d_%H%M%M)
+BACKUP_DIR="/data/LT/Backups/ClientsHowto"
+BACKUP_FILE="$BACKUP_DIR/register_${DATE}.db"
+
+# Safely extract a pristine snapshot using SQLite's backup engine
+sqlite3 /var/lib/langtechdepot/register.db ".backup '${BACKUP_FILE}'"
+
+# Keep the backup directory tidy: Delete snapshots older than 30 days
+find $BACKUP_DIR -name "register_*.db" -type f -mtime +30 -delete
+# set +x
