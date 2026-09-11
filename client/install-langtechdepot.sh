d9018c855a902f6f0b81969d79c4fabae8ddf81a
slsgzs-cloud <slsgzs@gmail.com>
2026-09-11 16:30:00 +0800

    fix: quote $BIN in ExecStart for systemd with spaces in HOME (#9)

--- client/install-langtechdepot.sh
+++ client/install-langtechdepot.sh
@@ -62,7 +62,7 @@ Description=LangTechDepot (Syncthing)
 After=network.target
 
 [Service]
-ExecStart=$BIN serve --no-browser --home "$CONFIG_DIR"
+ExecStart="$BIN" serve --no-browser --home "$CONFIG_DIR"
 Restart=on-failure
 
 [Install]
